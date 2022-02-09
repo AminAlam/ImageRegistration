e6bad437fd8bb5be4a352b1ff696ce020d95df04
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-02-09 16:52:33 +0330

    path of files are given by client in main body

--- NiiLoader.m
+++ NiiLoader.m
@@ -1,8 +1,9 @@
-function [V, V_label] = NiiLoader(i)
+function [V, V_label] = NiiLoader(i,path)
+
 if i<10
-    file_path = "./Project_Stuff/Datas/S0"+num2str(i)+"/";    
+    file_path = path+"S0"+num2str(i)+"/";    
 else
-    file_path = "./Project_Stuff/Datas/S"+num2str(i)+"/";    
+    file_path = path+"S"+num2str(i)+"/";    
 end
 
 V = double(niftiread(file_path+"pat"+num2str(i)+".nii"));
--- main.m
+++ main.m
@@ -4,13 +4,14 @@ clc
 clear
 
 % loading datas
-i = 6;
-[V, V_label] = NiiLoader(i);
+path = "./Project_Stuff/Datas/";
+i = 2;
+[V, V_label] = NiiLoader(i,path);
 tool = imtool3D((V));
 setMask(tool,(V_label));
 %% showing point clouds
 ptCloud = Pcloudmaker(V_label);
-pcshow(ptCloud)
+ax = pcshow(ptCloud);
 colormap jet
 
 %% registration
@@ -20,8 +21,8 @@ i = i + 1;
 [V2, V2_label] = NiiLoader(i);
 ptCloudMoving = Pcloudmaker(V1_label);
 ptCloudFixed = Pcloudmaker(V2_label);
-movingDownsampled = pcdownsample(ptCloudMoving,'gridAverage',2);
-fixedDownsampled = pcdownsample(ptCloudFixed,'gridAverage',2);
+movingDownsampled = pcdownsample(ptCloudMoving,'gridAverage',5);
+fixedDownsampled = pcdownsample(ptCloudFixed,'gridAverage',5);
 figure
 pcshowpair(movingDownsampled,fixedDownsampled,'MarkerSize',50)
 xlabel('X')
