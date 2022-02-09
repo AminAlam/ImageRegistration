de876c5ccda51444926fcd81a42ee569885e2940
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-02-09 16:52:44 +0330

    fianl ASD, validated

--- ASD.m
+++ ASD.m
@@ -3,10 +3,12 @@ function coeff = ASD(pCloudP,pCloudA)
     A = boundaryPC(pCloudA).Location;
     step = 10;
     dist = zeros(1,length(P));
+    
     for j = 1:step:length(P)
         p = P(j,:);
         A_temp = sqrt((A(:,2)-p(:,2)).^2+(A(:,1)-p(:,1)).^2+(A(:,3)-p(:,3)).^2);
-        dist(j) = sum(A_temp);
+        dist(j) = min(A_temp,[],'all');
     end
+    
     dist = sum(dist);
-coeff = 2*dist/(length(P)+length(A));
+    coeff = 2*dist/(length(P)+length(A));
