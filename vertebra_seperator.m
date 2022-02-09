8ce7890a80e46316a66dffdfb4802c3c5ddd2904
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-02-09 16:52:39 +0330

    a function for seperating each vertebras and doing all pre registration and ...

---
+++ vertebra_seperator.m
@@ -0,0 +1,19 @@
+function SeperateVertebras = vertebra_seperator(V_label,alpha_1, beta_1, alpha_2, GridStep)
+    Img = V_label;
+    VertebraNumbers = 15:1:30;
+    for i = VertebraNumbers
+        [x, y, z] = ind2sub(size(Img), find(Img == i));
+        xyzPoints = [x, y, z];
+        [row, col] = find(Img ~= 0);
+        ptCloud = pointCloud(xyzPoints);
+        if ~isempty(ptCloud.Location)
+            SeperateVertebras.(sprintf("Vertebra_%i", i)).ptCloud = ptCloud;
+            SeperateVertebras.(sprintf("Vertebra_%i", i)).number = i;
+            pc = PreRegister(ptCloud.Location,alpha_1, beta_1, alpha_2);
+            ptCloud_rotated = pointCloud(pc);
+            SeperateVertebras.(sprintf("Vertebra_%i", i)).ptCloud_rotated = ptCloud_rotated;
+            Boundary = boundaryPC(ptCloud_rotated);
+            SeperateVertebras.(sprintf("Vertebra_%i", i)).boundaryPointC = Boundary;
+            SeperateVertebras.(sprintf("Vertebra_%i", i)).sampledPC = pcdownsample(Boundary,'gridAverage',GridStep );
+        end
+end
