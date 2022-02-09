ffdc393bbca62d28de3ec2eb6fafce45ec51975f
MohammadAmin Alamalhoda <ma.alamalhoda@gmail.com>
2022-02-09 16:52:41 +0330

    ASD function, mostly correct but not sure

--- ASD.m
+++ ASD.m
@@ -1,10 +1,12 @@
-function coeff = ASD(img1,img2)
-
-    boundaries1 = BoundaryDetector(double(img1));
-    boundaries2 = BoundaryDetector(double(img2));
-    
-%     for k=1:length(boundaries)
-%         b = boundaries{k};
-%         plot(b(:,2),b(:,1),'g','LineWidth',3);
-%     end
-end
+function coeff = ASD(pCloudP,pCloudA)
+    P = boundaryPC(pCloudP).Location;
+    A = boundaryPC(pCloudA).Location;
+    step = 10;
+    dist = zeros(1,length(P));
+    for j = 1:step:length(P)
+        p = P(j,:);
+        A_temp = sqrt((A(:,2)-p(:,2)).^2+(A(:,1)-p(:,1)).^2+(A(:,3)-p(:,3)).^2);
+        dist(j) = sum(A_temp);
+    end
+    dist = sum(dist);
+coeff = 2*dist/(length(P)+length(A));
